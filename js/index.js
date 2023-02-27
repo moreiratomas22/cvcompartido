f5c7df7ab51f30fa9b8900fbc9444a29edf846d3
Tomas Pennisi Moreira <moreiratomas22@gmail.com>
2023-02-27 17:44:40 -0300

    Arreglos links y estilos

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-let info = {};
-const descripcion = document.getElementById("descripcion");
-const botonES = document.getElementsByClassName("boton-container")
+// let info = {};
+// const descripcion = document.getElementById("descripcion");
+// const botonES = document.getElementsByClassName("boton-container")
 
-const pedirInfo = () => {
-  fetch("./json/data.json")
-    .then((data) => {
-      //   console.log(data);
-      return data.json();
-      //   console.log(data.json())
-    })
-    .then((data) => {
-      info = data;
-      console.log(info);
-      descripcion.innerText = info.bio
-    });
+// const pedirInfo = () => {
+//   fetch("./json/data.json")
+//     .then((data) => {
+     
+//       return data.json();
+       
+//     })
+//     .then((data) => {
+//       info = data;
+//       console.log(info);
+//       descripcion.innerText = info.bio
+//     });
 
-};
+// };
 
 
-const changeButton = () => {
-    pedirInfo()
-    botonES[0].innerHTML= "<h3 id='ingles'>EN</h3>";
-    const botonEN = document.getElementById("ingles");
-console.log(botonEN)
-    botonEN.addEventListener("click", () => {
-        location.reload();
-    })
-}
+// const changeButton = () => {
+//     pedirInfo()
+//     botonES[0].innerHTML= "<h3 id='ingles'>EN</h3>";
+//     const botonEN = document.getElementById("ingles");
+// console.log(botonEN)
+//     botonEN.addEventListener("click", () => {
+//         location.reload();
+//     })
+// }
 
-const reloadButton = () => {
+// const reloadButton = () => {
     
-}
+// }
 
-botonES[0].addEventListener("click", changeButton)
+// botonES[0].addEventListener("click", changeButton)
